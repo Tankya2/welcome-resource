3d095a0770226709894bf3164db3ad0f70f66f6f
tkaiyong <sherb9@yahoo.com>
2020-05-04 23:27:36 +0800

    setup

--- src/python/orquestra/welcome.py
+++ src/python/orquestra/welcome.py
@@ -1,4 +1,15 @@
-#import system
-#print(sys.ver)
-import numpy as np
-print("Hello World 2")
+"""
+This module saves a welcome message.
+"""
+
+import json
+
+def welcome():
+    message = "Welcome to Orquestra!"
+
+    message_dict = {}
+    message_dict["message"] = message
+    message_dict["schema"] = "message"
+
+    with open("welcome.json",'w') as f:
+        f.write(json.dumps(message_dict, indent=2)) # Write message to file as this will serve as output artifact
